c155d0d49e82a2f455b5affeca55dbb130f5671a
Ammu <amruthaammu78355@gmail.com>
2025-11-04 13:10:39 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,10 +3,10 @@
 // The Firebase configuration will be populated by the Firebase CLI during deployment.
 // You can also manually add your Firebase project's configuration here.
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  projectId: "studio-7143942946-4608f",
+  appId: "1:701771061553:web:a3c96b8c3fddf06aae7810",
+  apiKey: "AIzaSyC5ZFZWE45vOqSAgSyLVfItmqEEGVXb1IA",
+  authDomain: "studio-7143942946-4608f.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "701771061553",
 };
