8c3212f133a57432b634b0a85d32505b73e9c094
Ammu <amruthaammu78355@gmail.com>
2026-01-19 15:54:44 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,3 @@
-
 // This file is intentionally left blank.
 // The Firebase configuration will be populated by the Firebase CLI during deployment.
 // You can also manually add your Firebase project's configuration here.
@@ -7,6 +6,7 @@ export const firebaseConfig = {
   appId: "1:701771061553:web:a3c96b8c3fddf06aae7810",
   apiKey: "AIzaSyC5ZFZWE45vOqSAgSyLVfItmqEEGVXb1IA",
   authDomain: "studio-7143942946-4608f.firebaseapp.com",
+  storageBucket: "studio-7143942946-4608f.appspot.com",
   measurementId: "",
   messagingSenderId: "701771061553",
 };
