330abd1d777f35fe49ce5b0673decd251b0da36d
Ammu <amruthaammu78355@gmail.com>
2026-02-04 09:53:37 +0000

    Error: Element type is invalid: expected a string (for built-in componen

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -7,7 +7,7 @@ import { DayPicker, useDayPicker, useNavigation } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./select"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
